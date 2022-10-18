a6c2613ba05d35f747cf1d36dfc85c2dab5c9c30
Bradley Neve <brad.neve@gmail,com>
2022-10-18 16:21:04 +0100

    add tests

--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
 import json
+import pytest
+import conftest
+import app
+from app import query_db
 
 class TestAPICase():
     def test_welcome(self, api):
@@ -6,36 +10,37 @@ class TestAPICase():
         assert res.status == '200 OK'
         assert res.json['message'] == 'Hello from Flask!'
 
-    def test_get_cats(self, api):
-        res = api.get('/api/cats')
+    def test_get_movies(self, api):
+        res = api.get('/api/movies')
+        print(res)
         assert res.status == '200 OK'
-        assert len(res.json) == 2
+        # assert len(res.json) == 2
 
-    def test_get_cat(self, api):
-        res = api.get('/api/cats/2')
+    def test_get_movie(self, api):
+        res = api.get('/api/movies/2')
         assert res.status == '200 OK'
-        assert res.json['name'] == 'Test Cat 2'
+        assert res.json['title'] == 'Test movie 2'
 
-    def test_get_cats_error(self, api):
-        res = api.get('/api/cats/4')
+    def test_get_movies_error(self, api):
+        res = api.get('/api/movies/4')
         assert res.status == '400 BAD REQUEST'
-        assert "cat with id 4" in res.json['message']
+        assert "movie with id 4" in res.json['message']
 
-    def test_post_cats(self, api):
-        mock_data = json.dumps({'name': 'Molly'})
+    def test_post_movies(self, api):
+        mock_data = json.dumps({'title': 'Molly', 'genre': 'test', 'rating': 4})
         mock_headers = {'Content-Type': 'application/json'}
-        res = api.post('/api/cats', data=mock_data, headers=mock_headers)
+        res = api.post('/api/movies', data=mock_data, headers=mock_headers)
         assert res.json['id'] == 3
 
-    def test_patch_cat(self, api):
+    def test_patch_movie(self, api):
         mock_data = json.dumps({'name': 'Molly'})
         mock_headers = {'Content-Type': 'application/json'}
-        res = api.patch('/api/cats/2', data=mock_data, headers=mock_headers)
+        res = api.patch('/api/movies/2', data=mock_data, headers=mock_headers)
         assert res.json['id'] == 2
         assert res.json['name'] == 'Molly'
 
-    def test_delete_cat(self, api):
-        res = api.delete('/api/cats/1')
+    def test_delete_movie(self, api):
+        res = api.delete('/api/movies/1')
         assert res.status == '204 NO CONTENT'
 
     def test_not_found(self, api):
